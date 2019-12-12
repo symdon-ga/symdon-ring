efa8a05544ae0ecffdb345201fe45968613719a5
sximada <50688746+sximada@users.noreply.github.com>
2019-12-12 13:55:18 +0900

    Fixed deploy scripts

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring "1.8.0"]
+                 [compojure "1.6.1"]]
   :main ^:skip-aot symdon-ring.core
   :repl-options {:init-ns symdon-ring.core}
   :profiles {:uberjar {:aot :all}})
--- src/symdon_ring/core.clj
+++ src/symdon_ring/core.clj
@@ -1,7 +1,35 @@
 (ns symdon-ring.core
-  (:gen-class))
+  (:gen-class)
+  (:require [compojure.core :refer [routes defroutes context GET]]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as server]
+            [ring.util.response :as res]))
+
+
+(defn handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defonce server (atom nil))
+
+(defn start-server []
+  (when-not @server
+    (reset! server (server/run-jetty #'handler {:port 3131 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (println "Starting...")
+  (start-server))
+
